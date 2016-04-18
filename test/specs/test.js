326cc26908977c34740214b7e5e9ef2f90b4ec3a
Латыпова Екатерина < >
2016-04-19 02:10:31 +0500

    исправления

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,18 +1,19 @@
 'use strict';
 
 const webdriverio = require('webdriverio');
-//const assert = require('assert');
+const url = 'http://panicky-car.surge.sh/';
 require('chai').should();
 
 var assert = require('assert');
 
 describe('Яндекс игра', function() {
+    beforeEach(() => {
+        browser.url(url);
+    });
     this.timeout(1000000);
 
     //  если нажимаем карту, то она становится видна
     it('should show card', function () {
-        browser.url('http://panicky-car.surge.sh/');
-
         browser.click('[for="c-0-0"]');
 
         let card = browser.element('[for="c-0-0"]');
@@ -21,83 +22,96 @@ describe('Яндекс игра', function() {
     });
 
     // если нажимаем две карты, но они не совпадают
-    it.skip('should return to start', function () {
-        browser.url('http://panicky-car.surge.sh/');
-
+    it('should return to start', function () {
         browser.click('[for="c-0-0"]');
         browser.click('[for="c-1-1"]');
 
         let section = browser.element('section:first-child');
         let visibility = section.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
+
+        let secondCard = browser.element('[for="c-1-1"]');
+        visibility = secondCard.getCssProperty('visibility').value;
+        visibility.should.be.equal('hidden');
     });
 
     // если нажимаем две карты, и они совпадают
     it('should show two card', function () {
-        browser.url('http://panicky-car.surge.sh/');
-
         browser.click('[for="c-0-0"]');
         browser.click('[for="heart"]');
 
-        let section = browser.element('[for="c-0-0"]');
-        let visibility = section.getCssProperty('visibility').value;
+        let firstCard = browser.element('[for="c-0-0"]');
+        let visibility = firstCard.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
 
-        let a = browser.element('[for="c-0-4"]');
-        let visibility1 = a.getCssProperty('visibility').value;
-        visibility1.should.be.equal('visible');
+        // тут хочется проверять псевдоэлемент [for="c-0-4"]:before
+        let secondCard = browser.element('[for="c-0-4"]');
+        visibility = secondCard.getCssProperty('visibility').value;
+        visibility.should.be.equal('visible');
     });
 
     /*  если нажимаем две карты, и они совпадают
      потом нажимаем еще две, но они не совпадают
      ♥ -> ♥ -> ♣ -> ♦
      */
-    it.only('should show two cards that clicked first', function () {
-        browser.url('http://panicky-car.surge.sh/');
-
-        browser.click('[for="c-0-0"]');
+    it('should show two cards that clicked first', function () {
         browser.click('[for="heart"]');
         browser.click('[for="c-0-1"]');
         browser.click('[for="c-2-2"]');
 
-        let section = browser.element('[for="c-0-1"]');
-        let visibility = section.getCssProperty('visibility').value;
+        // тут хочется проверять псевдоэлемент [for="c-0-0"]:before
+        let firstCard = browser.element('[for="c-0-0"]');
+        let visibility = firstCard.getCssProperty('visibility').value;
+        visibility.should.be.equal('visible');
+
+        // тут хочется проверять псевдоэлемент [for="c-0-4"]:before
+        let secondCard = browser.element('[for="c-0-4"]');
+        visibility = secondCard.getCssProperty('visibility').value;
+        visibility.should.be.equal('visible');
+
+        let thirdCard = browser.element('[for="c-0-1"]');
+        visibility = thirdCard.getCssProperty('visibility').value;
+        visibility.should.be.equal('visible');
+
+        let fourthCard = browser.element('[for="c-2-2"]');
+        visibility = fourthCard.getCssProperty('visibility').value;
         visibility.should.be.equal('hidden');
-
-        let a = browser.element('[for="c-2-2"]');
-        let visibility1 = a.getCssProperty('visibility').value;
-        visibility1.should.be.equal('hidden');
-
     });
 
     /*  если нажимаем две карты, и они совпадают
      потом нажимаем еще две, и они совпадают
      ♥ -> ♥ -> ♣ -> ♣
      */
-    it.only('should show two cards that clicked first', function () {
-        browser.url('http://panicky-car.surge.sh/');
-
-        browser.click('[for="c-0-0"]');
+    it.skip('should show two cards that clicked first', function () {
         browser.click('[for="heart"]');
         browser.click('[for="c-0-1"]');
         browser.click('[for="club"]');
 
-        let section = browser.element('[for="c-0-1"]');
-        let visibility = section.getCssProperty('visibility').value;
-        visibility.should.be.equal('hidden');
+        // тут хочется проверять псевдоэлемент [for="c-0-0"]:before
+        let firstCard = browser.element('[for="c-0-0"]');
+        let visibility = firstCard.getCssProperty('visibility').value;
+        visibility.should.be.equal('visible');
 
-        let a = browser.element('[for="club"]');
-        let visibility1 = a.getCssProperty('visibility').value;
-        visibility1.should.be.equal('hidden');
+        // тут хочется проверять псевдоэлемент [for="c-0-4"]:before
+        let secondCard = browser.element('[for="c-0-4"]');
+        visibility = secondCard.getCssProperty('visibility').value;
+        visibility.should.be.equal('visible');
 
+        // тут хочется проверять псевдоэлемент [for="c-0-1"]:before
+        let thirdCard = browser.element('[for="c-0-1"]');
+        visibility = thirdCard.getCssProperty('visibility').value;
+        visibility.should.be.equal('visible');
+
+        // тут хочется проверять псевдоэлемент [for="c-0-3"]:before
+        let fourthCard = browser.element('[for="c-0-3"]');
+        visibility = fourthCard.getCssProperty('visibility').value;
+        visibility.should.be.equal('visible');
     });
 
     /* если все карты угадали, то выигрываем
      ♥ -> ♥ -> ♣ -> ♣ -> ♦ -> ♦
      */
-    it('should show win text', function () {
-        browser.url('http://panicky-car.surge.sh/');
-
+    it.skip('should show win text', function () {
         browser.click('[for="c-0-0"]');
         browser.click('[for="heart"]');
         browser.click('[for="c-0-1"]');
