090814723616a5c0b2115ee2ac3d4aecb3c84c7a
Латыпова Екатерина < >
2016-04-19 23:04:31 +0500

    вынесла селекторы в файл

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,126 +1,114 @@
 'use strict';
 
-const webdriverio = require('webdriverio');
-const url = 'http://panicky-car.surge.sh/';
+const data = require('./data.json');
 require('chai').should();
-
-var assert = require('assert');
 
 describe('Яндекс игра', function() {
     beforeEach(() => {
-        browser.url(url);
+        browser.url(data.url);
     });
     this.timeout(1000000);
 
     //  если нажимаем карту, то она становится видна
     it('should show card', function () {
-        browser.click('[for="c-0-0"]');
+        browser.click(data.firstTest.card);
 
-        let card = browser.element('[for="c-0-0"]');
-        let opacity = card.getCssProperty('opacity').value;
+        const card = browser.element(data.firstTest.card);
+        const opacity = card.getCssProperty('opacity').value;
         opacity.should.be.equal(1);
     });
 
     // если нажимаем две карты, но они не совпадают
     it('should return to start', function () {
-        browser.click('[for="c-0-0"]');
-        browser.click('[for="c-1-1"]');
+        browser.click(data.secondTest.firstCard);
+        browser.click(data.secondTest.secondCard);
 
-        let section = browser.element('section:first-child');
+        const section = browser.element(data.secondTest.visibleCard);
         let visibility = section.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
 
-        let secondCard = browser.element('[for="c-1-1"]');
+        const secondCard = browser.element(data.secondTest.invisibleCard);
         visibility = secondCard.getCssProperty('visibility').value;
         visibility.should.be.equal('hidden');
     });
 
     // если нажимаем две карты, и они совпадают
     it('should show two card', function () {
-        browser.click('[for="c-0-0"]');
-        browser.click('[for="heart"]');
+        browser.click(data.thirdTest.firstCard);
+        browser.click(data.thirdTest.secondCard);
 
-        let firstCard = browser.element('[for="c-0-0"]');
+        const firstCard = browser.element(data.thirdTest.firstVisibleCard);
         let visibility = firstCard.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
 
-        // тут хочется проверять псевдоэлемент [for="c-0-4"]:before
-        let secondCard = browser.element('[for="c-0-4"]');
+        const secondCard = browser.element(data.thirdTest.secondVisibleCard);
         visibility = secondCard.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
     });
 
     /*  если нажимаем две карты, и они совпадают
      потом нажимаем еще две, но они не совпадают
-     ♥ -> ♥ -> ♣ -> ♦
+     пример: ♥ -> ♥ -> ♣ -> ♦
      */
     it('should show two cards that clicked first', function () {
-        browser.click('[for="heart"]');
-        browser.click('[for="c-0-1"]');
-        browser.click('[for="c-2-2"]');
+        browser.click(data.fourthTest.firstCard);
+        browser.click(data.fourthTest.secondCard);
+        browser.click(data.fourthTest.thirdCard);
+        browser.click(data.fourthTest.fourthCard);
 
-        // тут хочется проверять псевдоэлемент [for="c-0-0"]:before
-        let firstCard = browser.element('[for="c-0-0"]');
+        const firstCard = browser.element(data.fourthTest.firstVisibleCard);
         let visibility = firstCard.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
 
-        // тут хочется проверять псевдоэлемент [for="c-0-4"]:before
-        let secondCard = browser.element('[for="c-0-4"]');
+        const secondCard = browser.element(data.fourthTest.secondVisibleCard);
         visibility = secondCard.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
 
-        let thirdCard = browser.element('[for="c-0-1"]');
-        visibility = thirdCard.getCssProperty('visibility').value;
-        visibility.should.be.equal('visible');
-
-        let fourthCard = browser.element('[for="c-2-2"]');
+        const fourthCard = browser.element(data.fourthTest.invisibleCard);
         visibility = fourthCard.getCssProperty('visibility').value;
         visibility.should.be.equal('hidden');
     });
 
     /*  если нажимаем две карты, и они совпадают
      потом нажимаем еще две, и они совпадают
-     ♥ -> ♥ -> ♣ -> ♣
+     пример: ♥ -> ♥ -> ♣ -> ♣
      */
-    it.skip('should show two cards that clicked first', function () {
-        browser.click('[for="heart"]');
-        browser.click('[for="c-0-1"]');
-        browser.click('[for="club"]');
+    it('should show two cards that clicked first', function () {
+        browser.click(data.fifthTest.firstCard);
+        browser.click(data.fifthTest.secondCard);
+        browser.click(data.fifthTest.thirdCard);
+        browser.click(data.fifthTest.fourthCard);
 
-        // тут хочется проверять псевдоэлемент [for="c-0-0"]:before
-        let firstCard = browser.element('[for="c-0-0"]');
+        const firstCard = browser.element(data.fifthTest.firstVisibleCard);
         let visibility = firstCard.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
 
-        // тут хочется проверять псевдоэлемент [for="c-0-4"]:before
-        let secondCard = browser.element('[for="c-0-4"]');
+        const secondCard = browser.element(data.fifthTest.secondVisibleCard);
         visibility = secondCard.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
 
-        // тут хочется проверять псевдоэлемент [for="c-0-1"]:before
-        let thirdCard = browser.element('[for="c-0-1"]');
+        const thirdCard = browser.element(data.fifthTest.thirdVisibleCard);
         visibility = thirdCard.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
 
-        // тут хочется проверять псевдоэлемент [for="c-0-3"]:before
-        let fourthCard = browser.element('[for="c-0-3"]');
+        const fourthCard = browser.element(data.fifthTest.fourthVisibleCard);
         visibility = fourthCard.getCssProperty('visibility').value;
         visibility.should.be.equal('visible');
     });
 
     /* если все карты угадали, то выигрываем
-     ♥ -> ♥ -> ♣ -> ♣ -> ♦ -> ♦
+     пример: ♥ -> ♥ -> ♣ -> ♣ -> ♦ -> ♦
      */
-    it.skip('should show win text', function () {
-        browser.click('[for="c-0-0"]');
-        browser.click('[for="heart"]');
-        browser.click('[for="c-0-1"]');
-        browser.click('[for="club"]');
-        browser.click('[for="c-0-2"]');
-        browser.click('[for="diamond"]');
+    it('should show win text', function () {
+        browser.click(data.sixthTest.firstCard);
+        browser.click(data.sixthTest.secondCard);
+        browser.click(data.sixthTest.thirdCard);
+        browser.click(data.sixthTest.fourthCard);
+        browser.click(data.sixthTest.fifthCard);
+        browser.click(data.sixthTest.sixthCard);
 
-        let link = browser.element('<a>');
-        let opacity = link.getCssProperty('opacity').value;
+        const link = browser.element(data.sixthTest.visibleElement);
+        const opacity = link.getCssProperty('opacity').value;
         opacity.should.be.equal(1);
     });
 });
