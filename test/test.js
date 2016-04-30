bc26bd63cee0337addbafce80abf1cea49bcfcec
PolinaKoval <polina.koval.32@gmail.com>
2016-05-01 00:32:41 +0500

    правки

--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
 var webdriverio = require('webdriverio');
 var chai = require('chai');
 var expect = chai.expect;
+var url = 'http://panicky-car.surge.sh/';
+
+var cards = ['#c-0-0','#c-0-1','#c-0-2','#c-0-3','#c-0-4','#c-0-5'];
+var pair = ['#c-0-0', '#heart'];
+var notpair = ['#c-0-1', '#c-2-5'];
 
 describe('test game', function (){
     this.timeout(99999999);
@@ -20,7 +25,7 @@ describe('test game', function (){
 
     it('only first section should be visibility at start', function (done) {
         client
-            .url('http://panicky-car.surge.sh/')
+            .url(url)
             .getCssProperty('section:first-child', 'visibility')
             .then(function (result) {
                 expect(result.value).to.equal('visible');
@@ -55,8 +60,8 @@ describe('test game', function (){
     it('only one section should be visibility after click', function (done) {
         var countOfVisible = 0;
         client
-            .url('http://panicky-car.surge.sh/')
-            .click('[for="c-0-0"]')
+            .url(url)
+            .click(cards[0])
             .getCssProperty('section:first-child', 'visibility')
             .then(function (result) {
                 if(result.value ==='visible') {
@@ -107,8 +112,8 @@ describe('test game', function (){
 
     it('should show card onClick', function (done) {
         client
-            .url('http://panicky-car.surge.sh/')
-            .click('label[for="c-0-0"]')
+            .url(url)
+            .click(cards[0])
             .getCssProperty('label[for="c-0-0"]:before', 'opacity')
             .then(function (result) {
                 expect(result.value).to.equal(1);
@@ -118,9 +123,9 @@ describe('test game', function (){
 
     it('should save correct pairs', function (done) {
         client
-            .url('http://panicky-car.surge.sh/')
-            .click('label[for="c-0-0"]')
-            .click('label[for="heart')
+            .url(url)
+            .click(pair[0])
+            .click(pair[1])
             .getCssProperty('label[for="c-0-0"]:before', 'opacity')
             .then(function (result) {
                 expect(result.value).to.equal(1);
@@ -134,9 +139,9 @@ describe('test game', function (){
 
     it('should hide wrong pairs', function (done) {
         client
-            .url('http://panicky-car.surge.sh/')
-            .click('label[for="c-0-0"]')
-            .click('label[for="c-1-5]')
+            .url(url)
+            .click(notpair[0])
+            .click(notpair[0])
             .getCssProperty('label[for="c-0-0"]:before', 'opacity')
             .then(function (result) {
                 expect(result.value).to.equal(0);
@@ -150,19 +155,20 @@ describe('test game', function (){
 
     it('should hide wrong pairs but show cards which were open before', function (done) {
         client
-            .url('http://panicky-car.surge.sh/')
-            .click('label[for="c-0-0"]')
-            .click('label[for="heart')
-            .click('label[for="c-0-1"]')
-            .click('label[for="c-2-5"]')
+            .url(url)
+            .click(pair[0])
+            .click(pair[1])
+            .click(notpair[0])
+            .click(notpair[1])
+            .getCssProperty('label[for="c-0-1"]:before', 'opacity')
+            .then(function (result) {
+                expect(result.value).to.equal(0);
+            })
+            .getCssProperty('label[for="c-0-3"]:before', 'opacity')
+            .then(function (result) {
+                expect(result.value).to.equal(0);
+            })
             .getCssProperty('label[for="c-0-0"]:before', 'opacity')
-            .then(function (result) {
-                expect(result.value).to.equal(0);
-            })
-            .getCssProperty('label[for="c-0-4"]:before', 'opacity')
-            .then(function (result) {
-                expect(result.value).to.equal(0);
-            })
             .then(function (result) {
                 expect(result.value).to.equal(1);
             })
@@ -175,7 +181,7 @@ describe('test game', function (){
 
     it('should show win message if all pairs are open', function (done) {
         client
-            .url('http://panicky-car.surge.sh/')
+            .url(url)
             .click('#heart')
             .click('#club')
             .click('#diamond')
