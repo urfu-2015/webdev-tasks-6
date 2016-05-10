de946d9a812a4ae38f17de6660aaef5ce51d9d39
Anna1004 <anna.khlutkova@mail.ru>
2016-05-10 23:01:42 +0500

    Запоздалое решение.

---
+++ test/specs/test.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const webdriverio = require('webdriverio');
+const should = require('chai').should();
+const gameUrl = 'http://panicky-car.surge.sh/';
+
+describe('Test for card game', () => {
+
+    it('should not show cards when game starts', () => {
+        browser.url(gameUrl);
+
+        browser.element('label[for="c-1-0"]')
+            .getCssProperty('visibility').value.should.equal('hidden');
+        browser.element('label[for="c-2-0"]')
+            .getCssProperty('visibility').value.should.equal('hidden');
+        browser.element('label[for="c-3-0"]')
+            .getCssProperty('visibility').value.should.equal('hidden');
+        browser.element('label[for="c-4-0"]')
+            .getCssProperty('visibility').value.should.equal('hidden');
+        browser.element('label[for="c-5-0"]')
+            .getCssProperty('visibility').value.should.equal('hidden');
+        browser.element('label[for="c-6-0"]')
+            .getCssProperty('visibility').value.should.equal('hidden');
+    });
+
+    it('card should be visibled when click on it', () => {
+        browser.url(gameUrl);
+
+        browser.click('label[for="c-0-0"]');
+        browser.element('label[for="c-0-0"]')
+            .getCssProperty('visibility').value.should.equal('visible');
+    });
+
+    it('cards should are visible when there are the same content', () => {
+        browser.url(gameUrl);
+
+        browser.click('label[for="heart"]');
+        browser.click('label[for="c-0-0"]');
+        
+        browser.element('#heart').getAttribute('checked').should.equal('true');
+    });
+
+    it('correctly pair should be showed when next choice is wrong', () => {
+        browser.url(gameUrl);
+
+        browser.click('label[for="heart"]');
+        browser.click('label[for="c-0-0"]');
+        browser.click('label[for="c-0-2"]');
+        browser.click('label[for="c-3-1"]');
+
+        browser.element('#diamond').getAttribute('checked').should.equal(null);
+        browser.element('#heart').getAttribute('checked').should.equal(true);
+    });
+
+    it('win label should be showed when there are 3 right pair', () => {
+        browser.url(gameUrl);
+
+        browser.element('<a>').getCssProperty('opacity').value.should.equal(0);
+
+        browser.click('label[for="heart"]');
+        browser.click('label[for="c-0-0"]');
+        browser.click('label[for="club"]');
+        browser.click('label[for="c-0-1"]');
+        browser.click('label[for="diamond"]');
+        browser.click('label[for="c-0-2"]');
+
+        browser.element('<a>').getCssProperty('opacity').value.should.equal(1);    
+    });
+});
